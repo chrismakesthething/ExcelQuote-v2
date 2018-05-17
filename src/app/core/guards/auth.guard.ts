9fd45cb5c66a3b7658b691b524bec3e68bde85d6
chrismakesthething <anim4te@gmail.com>
2018-05-18 10:17:36 +1200

    Converted project into SCSS

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -8,7 +8,9 @@ import { AuthService } from '../services/auth.service';
 )
 export class AuthGuard implements CanActivate {
 
-    constructor(private Auth: AuthService, private Router: Router){}
+    constructor(private Auth: AuthService, private Router: Router){
+        
+    }
 
     canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
 
@@ -16,14 +18,14 @@ export class AuthGuard implements CanActivate {
         // let foo$ = this.Auth.currentUserObservable;
         // foo$.subscribe((res) => {console.log(res)})
         if (this.Auth.currentUser) {
-            console.log("Your logged in");
+            console.log('Your logged in');
             // console.log(this.Auth.currentUserObservable);
             // console.log(this.Auth.authenticated);
             return true;
         } else {
-            console.log("Your not logged in");
+            console.log('Your not logged in');
             // console.log(this.Auth.currentUserObservable);
-            // this.Router.navigate(['/login']);
+            this.Router.navigate(['/login']);
             return false;
         }
     }
