e72329c22ca613b9b0db5416ce02751a4dd7bc4c
chrismakesthething <anim4te@gmail.com>
2018-05-18 15:42:53 +1200

    scss

--- src/app/home/pages/login/login.component.ts
+++ src/app/home/pages/login/login.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../../core/services/auth.service';
 import { Router } from '@angular/router';
-
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
